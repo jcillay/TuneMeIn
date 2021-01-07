f5a4162c8b9a280aafeb5be533cfaff67c1fb500
jcillay <jcillay.5@gmail.com>
2021-01-06 19:20:26 -0800

    Just adding the urls for the views that we have written for the spotifyAPP

--- tunes_controller/spotify/urls.py
+++ tunes_controller/spotify/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 
-from .views import AUTHURL
+from .views import AUTHURL, spotify_callback
 
 urlpatterns = [
     path('get-auth-url', AUTHURL.as_view()),
+    path('redirect', spotify_callback)
 ]
