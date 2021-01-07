5833a278b43cfa26cdc95b01212f7005f8e0738f
jcillay <jcillay.5@gmail.com>
2021-01-06 23:21:58 -0800

    Added the credentials for our client id secret, and redirect uri

--- tunes_controller/spotify/credentials.py
+++ tunes_controller/spotify/credentials.py
@@ -1,4 +1,4 @@
 # Stores client id,the client secret and th euri
 CLIENT_ID = "a18ebca158b44b93b6ec16d73676c7bc"
 CLIENT_SECRET = "d86d485fc3b84b5b8dc0c1995bdb53fa"
-REDIRECT_URI = ""
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
